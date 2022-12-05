3884519268c4262153864442e90f415329bf6600
SnEG1R <steamsnegir@gmail.com>
2022-12-05 20:30:56 +0300

    add automatic deployment of migrations

--- Recommendation.Persistence/DependencyInjection.cs
+++ Recommendation.Persistence/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.Reflection;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.Google;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Recommendation.Application.Common.Constants;
 using Recommendation.Application.ConfigurationModels;
 using Recommendation.Application.Interfaces;
 using Recommendation.Domain;
@@ -16,18 +18,15 @@ namespace Recommendation.Persistence;
 
 public static class DependencyInjection
 {
-    private const string AspNetEnvironment = "ASPNETCORE_ENVIRONMENT";
-    private const string TypeEnvironment = "Production";
-
     public static IServiceCollection AddPersistence(this IServiceCollection services)
     {
         var serviceProvider = services.BuildServiceProvider();
         var connectionString = GetConnectionString(serviceProvider);
 
         services.AddDbContext<RecommendationDbContext>(options =>
-        {
-            options.UseNpgsql(connectionString);
-        });
+            options.UseNpgsql(connectionString, o =>
+                o.MigrationsAssembly(typeof(RecommendationDbContext).Assembly.FullName)));
+
         services.AddScoped<IRecommendationDbContext, RecommendationDbContext>();
         services.AddIdentityConfiguration();
         return services;
@@ -38,7 +37,8 @@ public static class DependencyInjection
         services.AddIdentity<User, IdentityRole>(options =>
             {
                 options.User.RequireUniqueEmail = true;
-                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+/ ";
+                options.User.AllowedUserNameCharacters =
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+/ ";
                 options.Password.RequiredLength = 5;
                 options.Password.RequireDigit = false;
                 options.Password.RequireLowercase = false;
@@ -52,8 +52,8 @@ public static class DependencyInjection
     private static string GetConnectionString(IServiceProvider serviceProvider)
     {
         var configuration = serviceProvider.GetRequiredService<ConnectionStringsConfiguration>();
-        var connectionString = Environment.GetEnvironmentVariable(AspNetEnvironment)
-                               == TypeEnvironment
+        var connectionString = Environment.GetEnvironmentVariable(EnvironmentConfiguration.AspNetEnvironment)
+                               == EnvironmentConfiguration.ProductionType
             ? configuration.RecommendationDbConnectionStringProduction
             : configuration.RecommendationDbConnectionStringDevelop;
 
