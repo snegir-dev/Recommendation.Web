08189140ec8fa0c34b59b29748b51477978f44e0
SnEG1R <steamsnegir@gmail.com>
2023-01-09 00:34:48 +0300

    add a display of the number of likes in the admin panel

--- Recommendation.Application/CQs/User/Queries/GetAllUser/GetAllUserDto.cs
+++ Recommendation.Application/CQs/User/Queries/GetAllUser/GetAllUserDto.cs
@@ -9,6 +9,7 @@ public class GetAllUserDto : IMapWith<UserApp>
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
+    public int UserLikeCount { get; set; }
     public string Role { get; set; }
     public string AccessStatus { get; set; }
 
@@ -20,6 +21,8 @@ public class GetAllUserDto : IMapWith<UserApp>
             .ForMember(u => u.Name,
                 c => c.MapFrom(u => u.UserName))
             .ForMember(u => u.Email,
-                c => c.MapFrom(u => u.Email));
+                c => c.MapFrom(u => u.Email))
+            .ForMember(u => u.UserLikeCount,
+                c => c.MapFrom(u => u.CountLike));
     }
 }
